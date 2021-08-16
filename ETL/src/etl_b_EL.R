cd84bbe037565c9102ddf98c7dd77cc5863c113b
Christopher Michaud <michaudc870@gmail.com>
2021-08-16 11:14:03 -0600

    building final data restructure and upload script

--- ETL/src/etl_b_EL.R
+++ ETL/src/etl_b_EL.R
@@ -11,8 +11,11 @@
   library(googledrive)
   library(UCRBtools)
   library(DBI)
+  library(bigrquery)
 }
 
+# Source bq write-safe
+source("~/Documents/etc/bq_write-safe.R")
 # build "exclude"
 `%!in%` <- Negate(`%in%`)
 
@@ -65,102 +68,115 @@ if (nrow(qc_sheet) == 0) {
 } else if (nrow(qc_sheet) > 1) {
   stop("Multiple QAQC sheets exist! Assess and remove all but one.")
 } else {
-  message("move code here")
+
+  # ----- Import proofed data from gsheets -----
+
+  s_names <- sheet_names(qc_sheet)
+
+  dat <- s_names %>%
+    map(~ read_sheet(qc_sheet, sheet = .))
+
+  names(dat) <- s_names
+
+  # ----- Data cleanup -----
+  fnl_dat <- dat %>%
+    #  map(~ modify_if(.x, is.POSIXct, as.character)) %>%
+    map(~ select(.x, -c(matches("_flg$|_index$|^key_"))))
+
+  # ----- Additional cleaning and structuring -----
+
+  # Water Quality
+  nst_water <- fnl_dat$water_qual %>%
+    select(SiteID = site_id,
+           AmbientConductivity_uS = cond_amb,
+           SpecificConductivity_uS = cond_spec,
+           WaterTemperature_C = rvr_temp,
+           SecchiDepth_mm = secchi,
+           Notes = water_notes) %>%
+    nest(WaterQuality = AmbientConductivity_uS:Notes)
+
+
+  # Pit Tag
+  nst_pittag <- fnl_dat$pittag %>%
+    mutate(PitTagType = as.character(pit_type),
+           IsPitTagRecapture = case_when(pit_recap == "Y" ~ TRUE,
+                                         pit_recap == "N" ~ FALSE)) %>%
+    select(SiteID = site_id,
+           PitTagString = pit_num,
+           IsPitTagRecapture,
+           PitTagType,
+           Notes = pit_notes) %>%
+    nest(PitTag = PitTagString:Notes)
+
+
+  # Floy Tag
+  nst_floytag <- fnl_dat$floytag %>%
+    mutate(IsFloyTagRecapture = case_when(floy_recap == "Y" ~ TRUE,
+                                          floy_recap == "N" ~ FALSE)) %>%
+    select(SiteID = site_id,
+           FloyTagString = floy_num,
+           FloyTagColorCode = floy_color,
+           IsFloyTagRecapture,
+           Notes = floy_notes) %>%
+    nest(FloyTag = FloyTagString:Notes)
+
+
+  # Fish Data
+  nst_fish <- fnl_dat$fish %>%
+    mutate(FishCount = 1,
+           IsRipe = case_when(grepl("^EXP", rep_cond) ~ TRUE,
+                              grepl("^INT|NOT", rep_cond) ~ FALSE),
+           IsTuberculate = case_when(species %in% spp_nat &
+                                       tubercles == "Y" ~ TRUE,
+                                     species %in% spp_nat &
+                                       tubercles == "N" ~ FALSE)) %>%
+    separate(ray_ct, into = c("DorsalRayCount", "AnalRayCount"), sep = "/") %>%
+    select(SiteID = site_id,
+           EncounterDateTime_UTC = datetime,
+           EncounterLocation_BelknapMiles = rmi,
+           SpeciesCode = species,
+           FishCount,
+           TotalLength_mm = tot_length,
+           Weight_g = weight,
+           Sex = sex,
+           IsRipe,
+           IsTuberculate,
+           DorsalRayCount,
+           AnalRayCount,
+           DispositionCode = disp,
+           Easting_UTM = loc_x,
+           Northing_UTM = loc_y,
+           EPSGCode = epsg,
+           Notes = fish_notes) %>%
+    left_join(nst_pittag, by = "SiteID") %>%
+    left_join(nst_floytag, by = "SiteID") %>%
+    nest(FishData = c(EncounterDateTime_UTC:FloyTag))
+
+
+  # Final nested site data
+  site <- fnl_dat$site %>%
+    select(SiteID = site_id,
+           StudyCode = project,
+           RiverCode = river,
+           PassIdentifier = pass,
+           StartLocation_BelknapMiles = start_rmi,
+           EndLocation_BelknapMiles = end_rmi,
+           StartDateTime_UTC = startdatetime,
+           EndDateTime_UTC = enddatetime,
+           Shoreline = shoreline,
+           Effort_Seconds = el_sec,
+           BoatName = boat,
+           CrewNames = crew,
+           Notes = site_notes) %>%
+    left_join(nst_water, by = "SiteID") %>%
+    left_join(nst_fish, by = "SiteID")
+
 }
 
 
-#---------------------------
-# Final data mods
-#---------------------------
+# Write data to Big Query
 
-
-# ----- Import proofed data from gsheets -----
-
-s_names <- sheet_names(qc_sheet)
-
-dat <- s_names %>%
-  map(~ read_sheet(qc_sheet, sheet = .))
-
-names(dat) <- s_names
-
-# ----- Data cleanup -----
-fnl_dat <- dat %>%
-#  map(~ modify_if(.x, is.POSIXct, as.character)) %>%
-  map(~ select(.x, -c(matches("_flg$|_index$|^key_"))))
-
-# ----- Additional cleaning -----
-site <- fnl_dat$site %>%
-  select(SiteID = site_id,
-         StudyCode = project,
-         RiverCode = river,
-         PassIdentifier = pass,
-         StartLocation_BelknapMiles = start_rmi,
-         EndLocation_BelknapMiles = end_rmi,
-         StartDateTime_UTC = startdatetime,
-         EndDateTime_UTC = enddatetime,
-         Shoreline = shoreline,
-         Effort_Seconds = el_sec,
-         BoatName = boat,
-         CrewNames = crew,
-         Notes = site_notes)
-
-nst_water <- fnl_dat$water_qual %>%
-  select(SiteID = site_id,
-         AmbientConductivity_uS = cond_amb,
-         SpecificConductivity_uS = cond_spec,
-         WaterTemperature_C = rvr_temp,
-         SecchiDepth_mm = secchi,
-         Notes = water_notes) %>%
-  nest(WaterQuality = AmbientConductivity_uS:Notes)
-fish <- fnl_dat$fish %>%
-  mutate(FishCount = 1,
-         IsRipe = case_when(grepl("^EXP", rep_cond) ~ TRUE,
-                            grepl("^INT|NOT", rep_cond) ~ FALSE),
-         IsTuberculate = case_when(species %in% spp_nat &
-                                     tubercles == "Y" ~ TRUE,
-                                   species %in% spp_nat &
-                                     tubercles == "N" ~ FALSE)) %>%
-  separate(ray_ct, into = c("DorsalRayCount", "AnalRayCount"), sep = "/") %>%
-  select(SiteID = site_id,
-         EncounterDateTime_UTC = datetime,
-         EncounterLocation_BelknapMiles = rmi,
-         SpeciesCode = species,
-         FishCount,
-         TotalLength_mm = tot_length,
-         Weight_g = weight,
-         Sex = sex,
-         IsRipe,
-         IsTuberculate,
-         DorsalRayCount,
-         AnalRayCount,
-         DispositionCode = disp,
-         Easting_UTM = loc_x,
-         Northing_UTM = loc_y,
-         EPSGCode = epsg,
-         Notes = fish_notes)
-
-
-
-
-nst_pittag <- fnl_dat$pittag %>%
-  mutate(PitTagType = as.character(pit_type),
-         IsPitTagRecapture = case_when(pit_recap == "Y" ~ TRUE,
-                                       pit_recap == "N" ~ FALSE)) %>%
-  select(SiteID = site_id,
-         PitTagString = pit_num,
-         IsPitTagRecapture,
-         PitTagType,
-         Notes = pit_notes) %>%
-  nest(PitData = PitTagString:Notes)
-
-
-
-
-fnl_dat$floytag <- fnl_dat$floytag %>%
-  select(-c(species, site_id))
-
-fnl_dat$meta <- fnl_dat$meta %>%
-  distinct()
-
-saveRDS(fnl_dat, file = paste0("./projects/", config$proj, "-", data_yr, "/output/final-data.Rds"))
+if (exists("site")) {
+  message("place code here")
+}
 
